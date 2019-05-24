d5b9b8f2b6bfd7ea4f63a80dd202a67bc542df43
xkrieg <xkrieg@gmail.com>
2019-05-24 15:24:23 +0900

    Specify that the reports must end with "_report.pdf"

--- generate_reports.py
+++ generate_reports.py
@@ -8,8 +8,8 @@ from slack_functions import mkdir
 
 #Activate Rscript
 def send_report_request(filename):
-    command = "".join(["/usr/local/bin/Rscript ","--vanilla ",
-                       "report_builder.R ",filename])
+    command = "".join(["/usr/local/bin/Rscript ", "--vanilla ",
+                       "report_builder.R ", filename])
     call(command, shell=True, stdin=None, stdout=None, stderr=None,
          close_fds=True, bufsize=0)
     
@@ -30,6 +30,6 @@ def test_result_request(class_name, project_name, pre_post):
     #Gather list of pdfs
     directory = "".join(['output/',class_name,"/",project_name,"/",pre_post,"_reports"])
     mkdir(directory)
-    list_of_reports = [pdfs for pdfs in os.listdir(directory) if pdfs.endswith('.pdf')]
+    list_of_reports = [pdfs for pdfs in os.listdir(directory) if pdfs.endswith('_report.pdf')]
     
     return(list_of_reports)
