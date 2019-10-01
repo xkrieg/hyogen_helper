6b4dd5020e35fdad4ce90205264dcc8259fac0f7
xkrieg <xkrieg@gmail.com>
2019-10-01 12:15:43 +0900

    Remove test and add function to slack_functions file

--- slack_functions.py
+++ slack_functions.py
@@ -109,3 +109,50 @@ def upload_reports(slack, user_map, directory, list_of_files):
                            title=title_text)
                        
     print("Successfully uploaded reports.")
+    
+def add_slack_topic(slack, class_name, userIdNameMap, channel_name, question):
+    
+    #Get list of students and emails
+    student_dict = pd.read_csv("".join(["resources/",class_name,"_grades.csv"]))
+    student_dict = list(student_dict['user'])
+    members = [key  for (key, value) in userIdNameMap.items() if value in student_dict]
+    
+    #Create channel
+    slack.channels.create(name)
+    
+    #Find channel id
+    channels = slack.channels.list().body['channels']
+    channel_id = [channel['id'] for channel in channels if channel['name'] == channel_name][0]
+    
+    #Invite members
+    for member in members:
+        try:
+            slack.channels.invite(channel = channel_id, user = member)
+        except Exception:
+            print(member, "is already in the channel")
+    
+    #Set purpose and post question
+    slack.channels.set_purpose(channel = channel_id, purpose = question)
+    slack.chat.post_message(channel = name, text = question, username='@xkrieg',
+                            as_user = True)
+    print("Added topic:", name)
+    
+def send_mpfi(slack, class_name, userIdNameMap):
+    
+    message = 'Hello! Please complete the MPFI sometime during class today. I really appreciate your help in making this class even better! http://www.mpfi.xanderkrieg.com'
+    
+    #Get list of students and emails
+    student_dict = pd.read_csv("".join(["resources/",class_name,"_grades.csv"]))
+    student_dict = student_dict.loc[student_dict['mpfi'] == 1]
+    student_dict = list(student_dict['user'])
+    members = [key  for (key, value) in userIdNameMap.items() if value in student_dict]
+    
+    #Distribute to members
+    for member in members:
+        try:
+            slack.chat.post_message(channel = member, text = message, username='@xkrieg',
+                                    as_user = True)
+        except Exception as e:
+            print(e)
+
+    print("Sent MPFI to", len(members), "participants via direct message.")
