869d7b171dbc36c4a7c9600abc7e5f3b73932678
xkrieg <xkrieg@gmail.com>
2019-11-05 16:29:24 +0900

    Add experimentation and examination files

---
+++ grade_model_training/examination.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+#Import libraries
+import pandas as pd
+import tensorflow as tf
+from tensorflow import test
+from keras.optimizers import Nadam, SGD, Adam, RMSprop
+from keras.callbacks import EarlyStopping
+import datetime
+import os
+
+#Local libraries
+from datahelpers.preprocess import preprocessing
+from experiment import get_available_gpus, gpu_select
+from model.model_archt import get_model
+
+if __name__ == "__main__":
+
+    #Suppress Tensorflow information
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    
+    time_sec = 10
+    X, y = preprocessing("grade_model_training/data/deep_learn_data.csv")
+    
+    #Force Use GPU?
+    gpu_select(False)
+    
+    #Get model
+    model = get_model(X[50:, :], y[50:, :], dense_one = 256, 
+                      dense_two = 256, dense_three = 256, activation = 'relu', 
+                      last_activation = 'sigmoid', initialize = False, 
+                      print_model = True)
+                      
+    #Compile model
+    model.compile(loss='mse', optimizer = Nadam(lr=.001),
+                  metrics=['mean_squared_error', 'mean_absolute_error', 
+                           'mean_absolute_percentage_error', 'cosine_proximity'])
+    
+    #Set early stopping
+    earlystop = EarlyStopping(monitor = 'categorical_accuracy', min_delta = 0.001,
+                              patience = 29, mode = 'auto')
+    
+    #Train model
+    out = model.fit(X, y, batch_size = 200, epochs = 1000, validation_split = .10,
+                    callbacks = [earlystop])
+                    
+    #Serialize weights to HDF5
+    current_date = str(datetime.datetime.now())
+    model.save_weights("_".join(["grade_model_training/model/weights", current_date, ".h5"]))
+    print("Saved model to disk")
+    
+    #Predict
+    y_pred = pd.DataFrame(model.predict(X[0:50, :].round(0)))
+    y_pred.columns = ["predicted_grade"]
+    
+    #Check
+    y_pred["actual_grade"] = y[0:50, :]
+    
+    print(y_pred)
---
+++ grade_model_training/experiment.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+#Set seed
+seed_no = 3223
+from numpy.random import seed
+seed(seed_no)
+from tensorflow import set_random_seed
+set_random_seed(seed_no)
+
+#Import libraries
+import numpy as np
+import pandas as pd
+import talos as ta
+import tensorflow as tf
+from talos.utils.gpu_utils import multi_gpu
+from talos.model.normalizers import lr_normalizer
+from sklearn.preprocessing import StandardScaler
+from tensorflow import test
+from tensorflow.python.client import device_lib
+from keras.optimizers import Adam, Nadam, RMSprop, SGD
+from keras.callbacks import EarlyStopping
+import os
+
+#Local libraries
+from model.model_archt import get_model
+from datahelpers.preprocess import preprocessing
+
+#Check gpu support
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return len([x.name for x in local_device_protos if x.device_type == 'GPU'])
+
+def gpu_select(implement = True):
+    
+    #Suppress Tensorflow information
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    
+    #Ensure GPU functioning
+    if implement == True:
+        if test.gpu_device_name():
+            print('Default GPU Device: {}'.format(test.gpu_device_name()))
+            if get_available_gpus() > 1: #Add multi-gpu support if available
+                model = multi_gpu(model)
+        else:
+            print("Please install GPU version of TF")
+            quit()
+
+def import_model(X_train, y_train, X_val, y_val, params):
+    
+    #Load model architecture
+    model = get_model(X_train, y_train, params['dense_one'], params['dense_two'],
+                      params['dense_three'], params['activation'],
+                      params['last_activation'], initialize = True)
+    
+    #Add multi-gpu support if available
+    if get_available_gpus() > 1:
+        model = multi_gpu(model)
+    
+    #Compile model
+    model.compile(loss = params['losses'], metrics=['mean_squared_error', 
+                 'mean_absolute_error', 'mean_absolute_percentage_error', 
+                 'cosine_proximity'],optimizer = params['optimizer'](lr=lr_normalizer(params['lr'], 
+                                                 params['optimizer'])))
+    
+    #Set early stopping
+    earlystop = EarlyStopping(monitor='categorical_accuracy', min_delta=0.0001,
+                              patience=28, mode='auto')
+    
+    #Train model
+    out = model.fit(X_train, y_train, batch_size = params['batch_size'],
+                    epochs = params['epochs'], validation_data=[X_val, y_val],
+                    verbose=0, callbacks = [earlystop])
+    
+    return out, model
+
+def run_experiment(time_sec, params, outfile):
+    X, y = preprocessing('grade_model_training/data/deep_learn_data.csv')
+    
+    #Set scan paramters
+    scan_object = ta.Scan(X, y, model = import_model, params = params, 
+                          dataset_name = "grade_model_training/output/results", 
+                          experiment_no = outfile, grid_downsample = .2)
+    
+    return scan_object
+    
+if __name__ == "__main__":
+    
+    #Suppress Tensorflow information
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    
+    for seconds in [3]:#, 45, 60]:
+    
+        #print("".join(["Number of Seconds: ", str(seconds)]))
+    
+        #Determine parameters
+        p = {'dense_one': [32, 64, 128, 256, 512, 1024],
+             'dense_two': [32, 64, 128, 256, 512, 1024],
+             'dense_three': [32, 64, 128, 256, 512, 1024],
+             'activation': ['relu'],
+             'last_activation': [''],
+             'losses': ['mse'],
+             'lr': [0.005, 0.01, .05, 0.1],
+             'optimizer': [Adam, Nadam, RMSprop, SGD],
+             'batch_size': [2, 10, 50, 200, 500],
+             'epochs': [200]}
+    
+        #Force Use GPU?
+        gpu_select(False)
+    
+        #Name experiment
+        expname = "_".join(["layer", str(seconds)])
+    
+        #Start experiment
+        scanned = run_experiment(seconds, p, expname)
+        r = ta.Reporting(scanned)
+        
+        #Create graph
+        call(['Rscript', 'data_helper/plot_gen.R', expname])
+        
+        #Display cursory results
+        print("The highest model achieved an accuracy of %s \n" % r.high('val_categorical_accuracy'))
+        print("The best identified parameters are as follows:")
+        print(r.data.ix[r.rounds2high('val_categorical_accuracy')])
