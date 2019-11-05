423850aac024cbd4acb8943de00476f5aa232626
xkrieg <xkrieg@gmail.com>
2019-11-05 16:28:23 +0900

    Add datahelpers

---
+++ grade_model_training/datahelpers/plot_gen.R
@@ -0,0 +1,64 @@
+#Import Libraries
+suppressMessages(library(tidyr))
+suppressMessages(library(dplyr))
+
+#Accept arguments from python script
+args = paste0("output/psynary_", commandArgs(trailingOnly=TRUE), ".csv")
+#args = paste0("output/psynary_", "depmedwin2_4", ".csv") #For testing
+
+#Load data
+df <- read.csv(args, header = T, stringsAsFactors = F)
+
+#Reformat to long data
+ldf <- df[,c("categorical_accuracy", "val_categorical_accuracy", "conv_neuron",
+             "dense_neuron","lr","optimizer","batch_size")]
+ldf <- gather(ldf, key = train_test, value = accuracy, -conv_neuron, -dense_neuron,
+             -lr, -optimizer, -batch_size)
+
+#Recode train/test
+ldf$train_test <- as.factor(recode(ldf$train_test,
+                            "categorical_accuracy" = "Training Data",
+                            "val_categorical_accuracy" = "Test Data"))
+ldf$train_test <- relevel(ldf$train_test, ref = "Training Data")
+
+#Recode optimizers
+ldf$optimizer <- recode(ldf$optimizer, "<class 'keras.optimizers.Nadam'>" = "Nadam",
+                                       "<class 'keras.optimizers.Adam'>" = "Adam",
+                                       "<class 'keras.optimizers.RMSprop'>" = "RMSprop",
+                                       "<class 'keras.optimizers.SGD'>" = "SGD")
+
+#Finish long format
+ldf <- gather(ldf, key = parameter, value = setting, -train_test, -accuracy)
+
+#Rename parameters
+ldf$parameter <- recode(ldf$parameter, "batch_size" = "Batch Size",
+                                       "conv_neuron" = "Convolutional Neuron",
+                                       "dense_neuron" = "Dense Neuron",
+                                       "lr" = "Learning Rate",
+                                       "optimizer" = "Optimizer")
+
+#Leading zeros
+longest_str <- max(nchar(ldf[ldf$parameter != "Optimizer", "setting"]))
+longest_str <- paste0("%0", as.character(longest_str), "s")
+ldf$setting <- sprintf(longest_str, ldf$setting)
+
+#Grpah title
+title <- sub("^([^.]*).*", "\\1", basename(args))
+
+#Render HTML version of report
+infile <- paste0(sub("^([^.]*).*", "\\1", args), ".html")
+rmarkdown::render(input = 'data_helper/result_template.Rmd', 
+                  output_format = "html_document", 
+                  output_file = paste0("../", infile),
+                  params = c(ldf = ldf, title = title),
+                  quiet = T)
+
+#Convert to pdf
+outfile <- paste0(sub("^([^.]*).*", "\\1", args), ".pdf")
+comm<-paste("wkhtmltopdf -O landscape -q ", infile, outfile, sep = " ")
+system(comm)
+
+#Cleanup
+unlink(infile)
+unlink(paste0(sub("^([^.]*).*", "\\1", args), "_files"), recursive=TRUE)
+rm(args, title, df, ldf, longest_str, infile, outfile, comm)
---
+++ grade_model_training/datahelpers/preprocess.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+#Import required libraries
+from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
+from progressbar import ProgressBar
+import pandas as pd
+import os
+import gc
+
+#Read relevant files and return X, y
+def preprocessing(file):
+
+    #Load file
+    df = pd.read_csv(file)
+    df = df.reindex(np.random.RandomState(seed=3223).permutation(df.index))
+
+    #Make X Window Stack
+    X = df.values[:,2:]
+    X = StandardScaler().fit_transform(X)
+    print(X.shape)
+    
+    #Extract likert outcomes
+    y = df.values[:, :1].astype('int8')
+    print(y.shape)
+    
+    return(X, y)
+
+#Simple test script
+if __name__ == "__main__":
+    preprocessing('grade_model_training/data/deep_learn_data.csv')
